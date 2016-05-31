8327d4d9787292c70952454a46739d76050971e3
Danny Arnold <despair.blue@gmail.com>
2016-05-31 12:35:20 +0200

    consider receiving next month's money early

--- src/Main.elm
+++ src/Main.elm
@@ -2,10 +2,10 @@ port module Main exposing (..)
 
 import Html exposing (..)
 import Html.App as Html
-import Html.Events exposing (onInput)
+import Html.Events exposing (onInput, onCheck)
 import Html.Attributes exposing (..)
 import Date exposing (Date)
-import Date.Extra.Core exposing (daysInMonth)
+import Date.Extra.Core exposing (daysInMonth, firstOfNextMonthDate)
 import Time exposing (Time, second)
 import String
 
@@ -25,20 +25,21 @@ main =
 
 
 type alias Model =
-    { day : Int
-    , date : Float
-    , ausgabe : Float
-    , sicherheit : Float
+    { date : Float
+    , livelihood : Float
+    , safety : Float
+    , receivedMoneyForNextMonth : Bool
     }
 
 
 init : Maybe Model -> ( Model, Cmd Msg )
 init savedModel =
-    let
-        f =
-            Debug.log "flags" savedModel
-    in
-        Maybe.withDefault (Model 0 0 0 0) savedModel ! []
+    case savedModel of
+        Just model ->
+            ( model, Cmd.none )
+
+        Nothing ->
+            ( (Model 0 0 0 False), Cmd.none )
 
 
 
@@ -47,8 +48,9 @@ init savedModel =
 
 type Msg
     = Tick Time
-    | ChangeAusgabe String
-    | ChangeSicherheit String
+    | ChangeLivelyhood String
+    | ChangeSafety String
+    | ChangeReceivedMoneyForNextMonth Bool
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -59,32 +61,29 @@ update action model =
                 newDate =
                     newTime
 
-                dayOfMonth =
-                    newDate
-                        |> Date.fromTime
-                        |> Date.day
-
                 newModel =
                     { model
-                        | day = dayOfMonth
-                        , date = newDate
+                        | date = newDate
                     }
             in
-                ( Debug.log "model" newModel, Cmd.none )
+                newModel ! []
 
-        ChangeAusgabe newAusgabe ->
+        ChangeLivelyhood newAusgabe ->
             let
                 newAusgabeFloat =
                     Result.withDefault 0 (String.toFloat newAusgabe)
             in
-                ( { model | ausgabe = newAusgabeFloat }, Cmd.none )
+                { model | livelihood = newAusgabeFloat } ! []
 
-        ChangeSicherheit newSicherheit ->
+        ChangeSafety newSicherheit ->
             let
                 newSicherheitFloat =
                     Result.withDefault 0 (String.toFloat newSicherheit)
             in
-                ( { model | sicherheit = newSicherheitFloat }, Cmd.none )
+                { model | safety = newSicherheitFloat } ! []
+
+        ChangeReceivedMoneyForNextMonth value ->
+            { model | receivedMoneyForNextMonth = value } ! []
 
 
 
@@ -115,6 +114,65 @@ subscriptions model =
 view : Model -> Html Msg
 view model =
     let
+        balance =
+            if model.receivedMoneyForNextMonth then
+                (calculateBalance model)
+                    + (calculateBalance { model | date = beginningOfNextMonth model.date })
+            else
+                calculateBalance model
+    in
+        div []
+            [ h1 [] [ text "Money" ]
+            , div []
+                [ label []
+                    [ text "Livelihood: "
+                    , input
+                        [ value <| toString <| model.livelihood
+                        , onInput ChangeLivelyhood
+                        ]
+                        []
+                    ]
+                ]
+            , div []
+                [ label []
+                    [ text "Safety: "
+                    , input
+                        [ value <| toString <| model.safety
+                        , onInput ChangeSafety
+                        ]
+                        []
+                    ]
+                ]
+            , div []
+                [ label []
+                    [ text "Money for next month already recieved: "
+                    , input
+                        [ type' "checkbox"
+                        , checked model.receivedMoneyForNextMonth
+                        , onCheck ChangeReceivedMoneyForNextMonth
+                        ]
+                        []
+                    ]
+                ]
+            , text <| toString <| balance
+            ]
+
+
+
+-- HELPER
+
+
+beginningOfNextMonth : Float -> Float
+beginningOfNextMonth date =
+    date
+        |> Date.fromTime
+        |> firstOfNextMonthDate
+        |> Date.toTime
+
+
+calculateBalance : Model -> Float
+calculateBalance model =
+    let
         date =
             Date.fromTime model.date
 
@@ -124,38 +182,16 @@ view model =
         month =
             Date.month date
 
+        day =
+            Date.day date
+
         daysThisMonth =
             toFloat <| daysInMonth year month
 
         daysLeftThisMonth =
-            daysThisMonth - toFloat model.day
+            daysThisMonth - toFloat day
 
         balance =
-            (daysLeftThisMonth / daysThisMonth) * model.ausgabe + model.sicherheit
+            (daysLeftThisMonth / daysThisMonth) * model.livelihood + model.safety
     in
-        div []
-            [ h1 [] [ text "Money" ]
-            , div []
-                [ label []
-                    [ text "Ausgabe: "
-                    , input
-                        [ value <| toString <| model.ausgabe
-                        , onInput ChangeAusgabe
-                        ]
-                        []
-                    ]
-                ]
-            , div []
-                [ label []
-                    [ text "Sicherheit: "
-                    , input
-                        [ value
-                            <| toString
-                            <| model.sicherheit
-                        , onInput ChangeSicherheit
-                        ]
-                        []
-                    ]
-                ]
-            , text <| toString <| balance
-            ]
+        balance
